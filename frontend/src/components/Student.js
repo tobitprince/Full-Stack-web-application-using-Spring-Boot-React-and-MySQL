0ec76679fc45aa56c5e3d8c9a0e33b15d7709849
tobitprince <prince.tobit@strathmore.edu>
2024-12-22 11:59:04 +0300

    Enhance Student component layout and add current students display

--- frontend/src/components/Student.js
+++ frontend/src/components/Student.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import { useState } from 'react';
-import { Button } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import PublishIcon from '@mui/icons-material/Publish';
 import { useEffect } from 'react';
 
@@ -33,6 +33,7 @@ export default function Student() {
     )
   },[])
   return (
+    <Container>
     <Box
       component="form"
       sx={{ '& > :not(style)': { m: 1} }}
@@ -40,7 +41,7 @@ export default function Student() {
       autoComplete="off"
     >
         <Paper elevation={3} style={paperStyle}>
-            <h1 style={{color:"blue"}}><u> Add Student</u></h1>
+            <h1 style={{color:"blue", textAlign:"center"}}>Add Student</h1>
       <TextField id="student-name" label="Student Name" variant="standard" fullWidth
       value={name}
       onChange={(e)=>setName(e.target.value)} />
@@ -48,10 +49,24 @@ export default function Student() {
       value={address}
       onChange={(e)=>setAddress(e.target.value)} />
       <br></br>
-      <Button variant="contained" endIcon={<PublishIcon />} onClick={handleClick}>
+      <Button variant="contained" endIcon={<PublishIcon />} onClick={handleClick} style={{marginTop:"10px", marginLeft:"450px"}}>
         Submit
       </Button>
       </Paper>
-    </Box>
+      </Box>
+      <Box>
+      <Paper elevation={3} style={paperStyle}>
+      <h1 style={{textAlign:"center"}}>Current Students</h1>
+        {students.map(student=>(
+          <Paper elevation={6} style={{margin:"10px", padding:"15px",textAlign:"left" }} key={student.id}>
+            Id:{student.id}<br/>
+            Name:{student.name}<br/>
+            Address:{student.address}
+          </Paper>
+        ))
+      }
+      </Paper>
+      </Box>
+      </Container>
   );
 }
